55b3f2404a18fbeb1a9b7223bc282890c453b094
mypersonalwebsiteabelasfaw <abel.f.asfaw@gmail.com>
2020-08-17 21:18:06 -0400

    second commit

--- table.py
+++ table.py
@@ -37,13 +37,28 @@ def choose_item(event): #retuns type of data
     global tuplee
     tuplee = lst.get(lst.curselection()[0])
     #return tuplee
-    #Title_bar.delete(0,END)
-    #Title_bar.insert(END,tuplee[1])
+    Title_bar.delete(0,END)
+    Title_bar.insert(END,tuplee[1])
+    
+    year_bar.delete(0,END)
+    year_bar.insert(END,tuplee[3])
+    
+    Author_bar.delete(0,END)
+    Author_bar.insert(END,tuplee[2])
+    
+    ISBN_bar.delete(0,END)
+    ISBN_bar.insert(END,tuplee[4])
 
 def remove():
     storage.delete_data("MyBooks.db",tuplee[0])
 
+
+def update_():
+    storage.update_data("MyBooks.db",tuplee[0],title_input.get(), author_input.get(),year_input.get(),ISBN_input.get())
+
 screen = tk.Tk()
+screen.title("Book Database")
+screen.config(bg='gray')
 tk.Label(screen, text="Title").grid(row=0)
 tk.Label(screen, text="Year").grid(row=1)
 tk.Label(screen, text = "Author").grid(row=0,column=2)
@@ -51,24 +66,28 @@ tk.Label(screen,text = "ISBN").grid(row=1,column=2)
 
 
 title_input = StringVar()
-Title_bar = tk.Entry(screen,text=title_input).grid(row=0, column=1)
+Title_bar = tk.Entry(screen,text=title_input, bg = 'orange')
+Title_bar.grid(row=0, column=1)
 
 year_input = StringVar()
-year_bar = tk.Entry(screen,text=year_input ).grid(row=1, column=1)
+year_bar = tk.Entry(screen,text=year_input , bg = 'orange')
+year_bar.grid(row=1, column=1)
 
 author_input = StringVar()
-Author_bar = tk.Entry(screen,text=author_input).grid(row=0, column=4)
+Author_bar = tk.Entry(screen,text=author_input, bg = 'orange')
+Author_bar.grid(row=0, column=4)
 
 ISBN_input= StringVar()
-ISBN_bar = tk.Entry(screen,text=ISBN_input).grid(row=1, column=4)
+ISBN_bar = tk.Entry(screen,text=ISBN_input, bg = 'orange')
+ISBN_bar.grid(row=1, column=4)
 
 
-tk.Button(screen, text = 'Add',command = add_Data).grid(row = 2, column = 1,sticky='n') 
-tk.Button(screen, text = 'Update').grid(row = 3, column = 1,sticky='n') 
-tk.Button(screen, text = 'Delete',command = remove).grid(row = 4,column = 1,sticky='n') 
-tk.Button(screen, text = 'View',command = lookup).grid(row = 5, column = 1,sticky='n') 
-tk.Button(screen, text = 'Search',command  = searchup).grid(row = 6, column = 1,sticky='n') 
-tk.Button(screen, text = 'Close').grid(row = 7,column = 1,sticky='n') 
+tk.Button(screen, text = 'Add',width = 15,command = add_Data , bg = 'yellow').grid(row = 2, column = 1,sticky='n') 
+tk.Button(screen, text = 'Update',width = 15,command = update_, bg = 'yellow').grid(row = 3, column = 1,sticky='n') 
+tk.Button(screen, text = 'Delete',width = 15,command = remove, bg = 'yellow').grid(row = 4,column = 1,sticky='n') 
+tk.Button(screen, text = 'View',width = 15,command = lookup, bg = 'yellow').grid(row = 5, column = 1,sticky='n') 
+tk.Button(screen, text = 'Search',width = 15,command  = searchup, bg = 'yellow').grid(row = 6, column = 1,sticky='n') 
+tk.Button(screen, text = 'Close',width = 15,command =screen.destroy, bg = 'yellow').grid(row = 7,column = 1,sticky='n') 
 
 
 
